3bd20db374a61b3ec07525435e7c93df0273908b
oyebayofemi <oyebayooluwafemi@gmail.com>
2023-12-05 13:42:37 -0500

    Initial Commit

---
+++ OluwafemiINFO6131Assignment2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OluwafemiINFO6131Assignment2
+//
+//  Created by oyebayo oluwafemi on 2023-12-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OluwafemiINFO6131Assignment2/OluwafemiINFO6131Assignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  OluwafemiINFO6131Assignment2App.swift
+//  OluwafemiINFO6131Assignment2
+//
+//  Created by oyebayo oluwafemi on 2023-12-05.
+//
+
+import SwiftUI
+
+@main
+struct OluwafemiINFO6131Assignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
